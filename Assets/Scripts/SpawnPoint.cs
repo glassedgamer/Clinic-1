b5e43e32a751a709e295b17a97208c834aebd17b
glassedgamer <glassedgamer05@gmail.com>
2026-01-27 14:01:39 -0500

    Assets & code adjustments
    tutorial assets and random spawn times

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -4,15 +4,21 @@ public class SpawnPoint : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
 
-    [SerializeField] float spawnDelay = 1f;
+    [SerializeField] float spawnDelayMin = 1f;
+    [SerializeField] float spawnDelayMax = 5f;
+    float randSpawnDelay;
 
     private void Start()
     {
-        InvokeRepeating("SpawnEnemy", spawnDelay, spawnDelay);
+        randSpawnDelay = Random.Range(spawnDelayMin, spawnDelayMax);
+        InvokeRepeating("SpawnEnemy", randSpawnDelay, randSpawnDelay);
     }
 
     void SpawnEnemy()
     {
         Instantiate(enemy, this.transform.position, Quaternion.identity);
+
+        randSpawnDelay = Random.Range(spawnDelayMin, spawnDelayMax);
+        print(randSpawnDelay);
     }
 }
